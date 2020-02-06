d607a2973098ff391d1c3794ff1e672a5e082c95
Ernesto Castellotti <erny.castell@gmail.com>
2020-02-06 17:57:51 +0100

    Add copyright info

--- source/drm.d
+++ source/drm.d
@@ -1,7 +1,26 @@
+/* 
+Copyright (C) 2020 Ernesto Castellotti <mail@ernestocastellotti.it>
+Copyright (C) 2020 Davide Repetto
+
+	This file is part of ScuolaDRMFree.
+
+    ScuolaDRMFree is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ScuolaDRMFree is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with ScuolaDRMFree.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 module drm;
 
 import util : ScuolaDB;
-import std.range.interfaces : InputRange;
 
 void removeDrm(uint magic, ubyte[] pdf) {
     import std.stdio : writeln;
